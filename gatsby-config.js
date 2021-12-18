847b2d6ab3130c3fee4fb99973e9f49a2f6852d9
rlussier <reganlussier@gmail.com>
2021-12-18 12:17:38 -0600

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,37 @@
 module.exports = {
+  // pathPrefix: "/reggielussier",
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "reggielussier",
+    title: `Reggie Lussier`,
+    description: `Actor Website`,
+    author: `Reggie Lussier`,
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        icon: "src/images/icon.png",
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    }, 
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `green`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/RL.png`, 
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
+    `gatsby-plugin-gatsby-cloud`,
   ],
-};
+
+}
